43b3de0ad1b4211165351c423d258853fd04e187
makessensetomestore <makessensetomestore@gmail.com>
2025-10-06 03:20:12 -0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,8 @@
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+   export const firebaseConfig = {
+      apiKey: "AIzaSyC5KGC4QboPryY6o4_nOt3sH_BW6QR5oFI",
+      authDomain: "rewrite-tool.firebaseapp.com",
+      projectId: "rewrite-tool",
+      storageBucket: "rewrite-tool.appspot.com",
+      messagingSenderId: "1027664518543",
+      appId: "1:1027664518543:web:b78b014edf6a51616ebffa"
+    };
